06738f9968a7622139d824efee8cb587cf1ea151
jean-philippe.jaeck <jean-philippe.jaeck@atos.net>
2017-05-17 11:24:31 +0200

    MAJ fichier pour TP2

--- test_TP2/ListeBillets.php
+++ test_TP2/ListeBillets.php
@@ -1,1 +1,16 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test 1</title>
+	<meta charset="utf-8" />
+	<style> 
+	</style>
+	
+</head>
+<body>
+
+
+                                 
+</body>
+</html>
